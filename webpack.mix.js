d68601f9c0d02781b8ece822481acbbebd3f2d4c
Vladimir Yazykov <neizerth@gmail.com>
2018-02-10 22:12:36 +0300

    general update

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,6 @@ mix.pug = require('laravel-mix-pug')
 
 mix
     .js('src/js/app.js', 'dist/app.js')
-    .sass('src/blocks/common.sass', 'dist/app.css')
+    .sass('src/sass/app.sass', 'dist/app.css')
     .pug('src/pages/*.pug', 'dist', {seeds:'src/seeds'})
     .setPublicPath('dist');
