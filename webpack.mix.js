525777911ad7b6b54fabe1dba00b635038d5e3e7
Vladimir Yazykov <neizerth@gmail.com>
2018-02-11 23:19:19 +0300

    some header improvements

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
 let mix = require('laravel-mix');
 mix.pug = require('laravel-mix-pug')
 
+mix.setPublicPath('dist');
 mix
     .js('src/js/app.js', 'dist/app.js')
     .sass('src/sass/app.sass', 'dist/app.css')
     .pug('src/pages/*.pug', 'dist', {seeds:'src/seeds'})
-    .setPublicPath('dist');
